366749f8fcf7b8b3ebde86978b02e74bacb4de28
mg4417 <86538521+mg4417@users.noreply.github.com>
2021-06-28 05:26:17 +0100

    Delete reinvent-2/running_modes directory

--- reinvent-2/running_modes/configurations/scoring/scoring_runner_components.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class ScoringRunnerComponents:
-    """This class holds the necessary configuration components to do a scoring run."""
-
-    scoring: dict
-    scoring_function: dict
